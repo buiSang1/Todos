1b5b55c63ec98e17b7dacf36bf2b89d20e9d817e
Bui Sang <buisang785@gmail.com>
2022-06-28 21:53:37 +0700

    push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todos';
+  title = 'Todos';
+  todos = [
+    {
+    label: 'Sang',
+
+   },
+   {
+    label: 'MRSang',
+
+   }
+  ]
+
+  addTodo(newTodoLabel: any){
+    var newTodo = {
+      label: newTodoLabel,
+      done: false
+    };
+    this.todos.push(newTodo);
+  }
+  deleteTodo(todo: any){
+   this.todos =  this.todos.filter(t => t.label !== todo.label);
+  }
 }
+
+
